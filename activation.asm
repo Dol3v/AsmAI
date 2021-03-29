8810b725e13f8f1f3a42c243332b5d308a40200d
Dol3v <dol12dab@gmail.com>
2021-03-29 14:10:39 +0300

    added ReLu activation function

--- activation.asm
+++ activation.asm
@@ -1,2 +1,15 @@
 
 ; Contains all of the used activation functions
+
+section .text
+
+    ; Calculates the ReLu activation function:
+    ; 
+    ; ReLu(x) = max(x, 0)
+    ;
+    ; param %1: input/output
+    ; param %2: helper
+    %macro RELU 2
+        vpxor %2, %2 ;zero helper
+        vblendvpd %1, %1, %2, %1 ;if the sign bit is 1, the number is negative hence we move 0.0 to it
+    %endmacro
