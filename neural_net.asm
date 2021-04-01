f6508d35d7ea87847c525cfbbab3b6831463ede5
Dol3v <dol12dab@gmail.com>
2021-04-02 00:12:43 +0300

    replaced BINARY_NEURAL_NETWORK with variables

--- neural_net.asm
+++ neural_net.asm
@@ -1,30 +1,49 @@
 
 ; Functions that affect the whole neural network.
 
-%include "util.asm"
+%include "forward_prop.asm"
+%include "activation.asm"
 
-; Defines a four-layered network.
-;
-; Note: only one neural net can be declared.
-; 
-; All four parameters define the size of the different layers:
-; param 1: number of input values, divided by 4
-; param 2: number of values in first hidden layer, divided by 4
-; param 3: number of values in second hidden layer, divided by 4
-; Number of outputs is constant and equal to two.
-%macro BINARY_NEURAL_NET 3
-    nueral_net times %1*4*YMM_BYTE_LENGTH dq 0 ;input layer
-    times %1*4*YMM_BYTE_LENGTH*%2*4 dq 0 ;weights
-    times %2*4*YMM_BYTE_LENGTH dq 0 ;biases
-    times %2*4*YMM_BYTE_LENGTH dq 0 ;outputs, without activation
 
-    times %2*4*YMM_BYTE_LENGTH dq 0 ;first hidden layer inputs
-    times %2*4*YMM_BYTE_LENGTH*%3*4 dq 0 ;weights
-    times %3*4*YMM_BYTE_LENGTH dq 0 ;biases
-    times %3*4*YMM_BYTE_LENGTH dq 0 ;outputs, without activation
+INPUT_SIZE equ 4
+FIRST_HIDDEN_LAYER_SIZE equ 8
+SECOND_HIDDEN_LAYER_SIZE equ 4
+OUTPUT_SIZE equ 2
 
-    times %3*4*YMM_BYTE_LENGTH dq 0 ;output layer inputs
-    times %3*4*YMM_BYTE_LENGTH*2 dq 0 ;weights
-    times 2*YMM_BYTE_LENGTH dq 0 ;outputs, without activation
-    times 2*YMM_BYTE_LENGTH dq 0 ;neural net output
-%endmacro
+section .data
+    nueral_net times INPUT_SIZE*4*YMM_BYTE_LENGTH dq 0 ;input layer
+    nn_weights_1 times INPUT_SIZE*4*YMM_BYTE_LENGTH*FIRST_HIDDEN_LAYER_SIZE*4 dq 0 ;weights
+    nn_biases_1 times FIRST_HIDDEN_LAYER_SIZE*4*YMM_BYTE_LENGTH dq 0 ;biases
+    nn_zs_1 times FIRST_HIDDEN_LAYER_SIZE*4*YMM_BYTE_LENGTH dq 0 ;outputs, without activation
+
+    nn_inputs_2 times FIRST_HIDDEN_LAYER_SIZE*4*YMM_BYTE_LENGTH dq 0 ;first hidden layer inputs
+    nn_weights_2 times FIRST_HIDDEN_LAYER_SIZE*4*YMM_BYTE_LENGTH*SECOND_HIDDEN_LAYER_SIZE*4 dq 0 ;weights
+    nn_biases_2 times SECOND_HIDDEN_LAYER_SIZE*4*YMM_BYTE_LENGTH dq 0 ;biases
+    nn_zs_2 times SECOND_HIDDEN_LAYER_SIZE*4*YMM_BYTE_LENGTH dq 0 ;outputs, without activation
+
+    nn_inputs_3 times SECOND_HIDDEN_LAYER_SIZE*4*YMM_BYTE_LENGTH dq 0 ;output layer inputs
+    nn_weights_3 times SECOND_HIDDEN_LAYER_SIZE*4*YMM_BYTE_LENGTH*2 dq 0 ;weights
+    nn_biases_3 times OUTPUT_SIZE*YMM_BYTE_LENGTH dq 0 ;outputs, without activation
+    nn_output times OUTPUT_SIZE*YMM_BYTE_LENGTH dq 0 ;neural net output
+
+section .text
+
+    ; Forward-propagates a neural net.
+    ;
+    ; *Assumes used activation function is Sigmoid.
+    ;
+    ; param 1: input_size offset
+    ; param 2: neural_net offset
+    ; see: BINARY_NEURAL_NET macro
+    ForwardPropagateNetwork:
+        PUSHREGS
+
+        mov rbx, [rbp+8*3] ;input_sizes offset
+        mov rax, [rbx] ;input size
+        mov rcx, [rbx+8] ;layer 1 output size
+
+        mov rdi, [rbp+8*2] ;inputs offset
+        mov rsi, rdi ;TODO
+
+        POPREGS
+    ret 2*8
