e899fdc7aabd65d207d2fb818c5ff63f5998c613
Dol3v <dol12dab@gmail.com>
2021-04-11 22:20:07 +0300

    added READ_FILE macro

--- data_set.asm
+++ data_set.asm
@@ -6,6 +6,8 @@ OPEN_READ_ONLY_FLAG equ 0x00
 
 CLOSE_FILE_SYSCALL equ 3
 
+READ_FILE_SYSCALL equ 0
+
 section .data
 
 dataFileDescriptor dq 0
@@ -44,6 +46,35 @@ section .text
     mov rdi, %1
     syscall
 
+    pop rdi
+    pop rax
+%endmacro
+
+; Reads from a file.
+;
+; param: file descriptor
+; param: buffer offset
+; param: byte count - register
+%macro READ_FILE 3
+    push rax
+    push rdi
+    push rdx
+    push rsi
+
+    mov rax, READ_FILE_SYSCALL
+    mov rdi, %1
+    mov rsi, %2
+    mov rdx, %3
+    syscall
+
+    cmp rax, -1
+    je %%error_occured
+
+%%error_occured:
+    %error "There was an error reading the file."
+
+    pop rsi
+    pop rdx
     pop rdi
     pop rax
 %endmacro
